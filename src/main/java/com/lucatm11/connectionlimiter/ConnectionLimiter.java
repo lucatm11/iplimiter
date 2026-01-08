8cc59d9a618498c666261ef305b6bead35abe266
Luca <lightlucc@gmail.com>
2026-01-08 12:21:32 +0200

    Fix reload

--- src/main/java/com/lucatm11/connectionlimiter/ConnectionLimiter.java
+++ src/main/java/com/lucatm11/connectionlimiter/ConnectionLimiter.java
@@ -3,7 +3,6 @@ package com.lucatm11.connectionlimiter;
 import com.lucatm11.connectionlimiter.utils.configurations.Messages;
 import com.lucatm11.connectionlimiter.utils.configurations.Settings;
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -16,20 +15,10 @@ import com.lucatm11.connectionlimiter.utils.Connection;
 
 public class ConnectionLimiter extends JavaPlugin {
   public Connection connection;
-  private YamlConfiguration settingsConfig;
-  private YamlConfiguration messagesConfig;
   public Configurations.Messages messages;
   public Configurations.Settings settings;
 
   public void onEnable() {
-    Settings settingsLoader = new Settings(this);
-    settingsLoader.load();
-    settingsConfig = settingsLoader.getConfig();
-
-    Messages messagesLoader = new Messages(this);
-    messagesLoader.load();
-    messagesConfig = messagesLoader.getConfig();
-
     loadConfiguration();
 
     @SuppressWarnings("unused")
@@ -52,8 +41,15 @@ public class ConnectionLimiter extends JavaPlugin {
   }
 
   public void loadConfiguration() {
+    Settings settingsLoader = new Settings(this);
+    Messages messagesLoader = new Messages(this);
+
+    settingsLoader.load();
+    messagesLoader.load();
+
     Configurations configurations = new Configurations();
-    settings = configurations.new Settings(settingsConfig);
-    messages = configurations.new Messages(messagesConfig);
+
+    settings = configurations.new Settings(settingsLoader.getConfig());
+    messages = configurations.new Messages(messagesLoader.getConfig());
   }
 }
